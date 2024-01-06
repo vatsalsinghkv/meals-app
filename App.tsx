a80cf14138e76a7d5219064c5438b2b46c0a043e
vatsal <vatsalsinghkv@gmail.com>
2024-01-06 12:29:34 +0530

    feat: dynamic header title

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
+import { CATEGORIES, MEALS } from './src/lib/utils/data';
 import { Categories, Meals } from './src/screens';
 import Meal from './src/screens/Meal';
 
@@ -20,8 +21,29 @@ export default function App() {
           }}
         >
           <Stack.Screen name='Categories' component={Categories} />
-          <Stack.Screen name='Meals' component={Meals} />
-          <Stack.Screen name='Meal' component={Meal} />
+          <Stack.Screen
+            name='Meals'
+            component={Meals}
+            options={({ route }) => {
+              const { categoryId } = route.params as { categoryId: string };
+              const category = CATEGORIES.find((c) => c.id === categoryId);
+              return {
+                title: category?.title ?? 'meals',
+              };
+            }}
+          />
+          <Stack.Screen
+            name='Meal'
+            component={Meal}
+            options={({ route }) => {
+              const { id } = route.params as { id: string };
+              const meal = MEALS.find((m) => m.id === id);
+              return {
+                title: meal?.title ?? 'meal',
+                headerBackTitle: 'Back',
+              };
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
       <StatusBar style='dark' />
